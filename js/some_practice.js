f05c3ccafda7f7115e164d5e9d61cb2d4827adb9
MorozOleh <64013457+MorozOleh@users.noreply.github.com>
2020-08-28 11:50:07 +0300

    commite 2

---
+++ js/some_practice.js
@@ -0,0 +1,15 @@
+// const car = { make: 'Lada', model: 'Seven', year: 1991 };
+
+// console.log(typeof car);
+
+// console.log('make' in car);
+
+// delete car.make;
+
+// console.log(car);
+
+// if ('typeOfFuel' in car === false) {
+//   car.make = 'Petrol';
+// }
+
+// console.log(car);
--- js/task_5_hm_03.js
+++ js/task_5_hm_03.js
@@ -34,13 +34,45 @@
 
 // ---------------------------------------------------------------------
 
+// function getAllPropValues(array, prop) {
+//   let result = [];
+//   for (let i = 0; i < array.length; i += 1) {
+//     for (const key in array[i]) {
+//       if (key === prop) {
+//         result.push(array[i][key]);
+//       }
+//     }
+//   }
+//   return result;
+// }
+
+// // Объекты и ожидаемый результат
+
+// const products = [
+//   { name: 'Радар', price: 1300, quantity: 4 },
+//   { name: 'Радар', price: 1280, quantity: 2 },
+//   { name: 'Радар', price: 1320, quantity: 1 },
+//   { name: 'Сканер', price: 2700, quantity: 1 },
+//   { name: 'Сканер', price: 2500, quantity: 3 },
+//   { name: 'Дроид', price: 400, quantity: 7 },
+//   { name: 'Захват', price: 1200, quantity: 2 },
+// ];
+
+// console.log(getAllPropValues(products, 'name'));
+// // ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+// console.log(getAllPropValues(products, 'quantity'));
+// // [4, 2, 1, 1, 3, 7, 2];
+
+// console.log(getAllPropValues(products, 'category'));
+// //  []
+
+//-----------------------------------------------------------------------
+
 function getAllPropValues(array, prop) {
   let result = [];
   for (let i = 0; i < array.length; i += 1) {
-    for (const key in array[i]) {
-      if (key === prop) {
-        result.push(array[i][key]);
-      }
+    if (prop in array[i]) {
+      result.push(array[i][prop]);
     }
   }
   return result;
